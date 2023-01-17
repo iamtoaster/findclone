b2790d50ccb29a32faa33d6bcb23f2a30fe5f3b6
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2023-01-17 14:21:22 +0900

    Turned recursion into a loop

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,8 @@ pub struct AppArgs {
     /// A path from which the search will start
     start_path: String,
     /// Only paths that match this regex will be returned
-    regex: String
+    regex: String,
+
 }
 
 pub fn run(args: AppArgs) -> Result<(), Box<dyn Error>> {
@@ -26,34 +27,47 @@ pub fn run(args: AppArgs) -> Result<(), Box<dyn Error>> {
 /// Recursively goes through the directory and prints all directories /files
 /// that match the supplied regex.
 fn traverse_dir(dir: ReadDir, regex: &Regex) {
-    // Go through the directory contents
-    for i in dir {
-        match i {
-            Ok(entry) => {
-                match entry.file_type() {
-                    Ok(file_type) => {
-                        // If the file is a directory, we recursively go there too
-                        if file_type.is_dir() {
-                            match read_dir(entry.path()) {
-                                Ok(new_dir) => {
-                                    traverse_dir(new_dir, &regex)
-                                },
-                                Err(err) => println!("Failed to open directory {:?} with following error: {}, skipping.", entry.path(), err),
-                            }
-                        } else {
-                            if let Some(path) = entry.path().to_str() {
-                                if regex.is_match(path) {
-                                    println!("{}", path.to_string());
+    let mut dirs = vec![dir];
+
+    loop {
+        if dirs.len() == 0 {
+            break;
+        }
+
+        let dir = match dirs.pop() {
+            Some(e) => e,
+            None => continue,
+        };
+
+        // Go through the directory contents
+        for i in dir {
+            match i {
+                Ok(entry) => {
+                    match entry.file_type() {
+                        Ok(file_type) => {
+                            // If the file is a directory, we recursively go there too
+                            if file_type.is_dir() {
+                                match read_dir(entry.path()) {
+                                    Ok(new_dir) => {
+                                        dirs.push(new_dir);
+                                    },
+                                    Err(err) => println!("findclone: {:?}: {}", entry.path(), err),
+                                }
+                            } else {
+                                if let Some(path) = entry.path().to_str() {
+                                    if regex.is_match(path) {
+                                        println!("{}", path.to_string());
+                                    }
                                 }
                             }
-                        }
-                    },
-                    Err(err) => println!("Failed to get file type with following error: {:?}, skipping.", err),
-                }
-            },
-            Err(err) => {
-                println!("Failed to read directory entry with following error: {:?}, skipping.", err);
-            },
+                        },
+                        Err(err) => println!("findclone: {:?}: {}", entry.path(), err),
+                    }
+                },
+                Err(err) => {
+                    println!("findclone: {}", err);
+                },
+            }
         }
     }
 }
